613d65ea0ef1931dbdffdfafd1b9ce47030f9bcf
Andrew Taghi <aataghi13@gmail.com>
2020-07-09 14:05:53 -0500

    Added ReadME Info

--- models/note.php
+++ models/note.php
@@ -1,24 +1,6 @@
 
 <?php
-$dbconn = null;
-if(getenv('DATABASE_URL')){
-$connectionConfig = parse_url(getenv('DATABASE_URL'));
-$host = $connectionConfig['host'];
-$user = $connectionConfig['user'];
-$password = $connectionConfig['pass'];
-$port = $connectionConfig['port'];
-$dbname = trim($connectionConfig['path'],'/');
-$dbconn = pg_connect(
-"host=".$host." ".
-"user=".$user." ".
-"password=".$password." ".
-"port=".$port." ".
-"dbname=".$dbname
-);
-} else {
-$dbconn = pg_connect("host=localhost dbname=notes port=6543");
-}
-
+$dbconn = pg_connect("host=localhost dbname=notes");
 
 class Note {
   public $id;
